f2a4cc6751b4be854b323cecff48be9903d1dfb3
chrpkorir <faythkoko@gmail.com>
2021-01-27 00:50:09 -0800

    ADD print files

---
+++ input.py
@@ -0,0 +1,12 @@
+
+# The input funciton allows you to prompt the user for a value
+
+# You need to declare a variable to hold the value entered by the user
+Name = input('What is your name? ')
+
+age = input('How old are you?')
+
+
+course = input('which course are you pursuing?')
+
+print('Great, your name is '+ Name + ' And you are ' + age + ' yrs old' ' Pursuing '+ course) 
