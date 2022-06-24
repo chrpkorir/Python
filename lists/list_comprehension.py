91153e97e0d8ad8466e73dd38992b1c938850989
chrpkorir <faythkoko@gmail.com>
2022-06-24 15:17:10 +0000

    List comprehension

---
+++ lists/list_comprehension.py
@@ -0,0 +1,10 @@
+# List comprehensions are a way to 
+# make new lists where each element
+# is the result of some operations applied 
+# to each memmber of another sequence
+
+squares = []
+for x in range(10):
+    squares.append(x**2)
+
+print(squares)    
