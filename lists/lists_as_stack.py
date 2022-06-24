4c40a9d9fbebd592508e14a41d3d75768917e795
chrpkorir <faythkoko@gmail.com>
2022-06-24 15:21:52 +0000

    Lists as stack

---
+++ lists/lists_as_stack.py
@@ -0,0 +1,15 @@
+# To use list a stack the last element
+# added is the first element retrieved
+# (LIFO)
+
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+
+print(stack)
+stack.pop()
+
+print(stack)
+stack.pop()
+
+print(stack)
