dd3cc4633764f57ebbbb4e647edfc19718905227
chrpkorir <faythkoko@gmail.com>
2022-06-27 09:39:31 +0000

    Encapsulation

--- Object_oriented_programming/obj.py
+++ Object_oriented_programming/obj.py
@@ -1,11 +1,26 @@
-def hi(obj):
-    print("Hello, I am" + obj.name + "!")
+
+"""def hi(obj):
+    print("Hello, I am" + obj.name)
 
 
 class Robot:
-    pass
+    say_hi = hi  # bind function hi to class attribute say_hi
 
 
 x = Robot()
 x.name = "Marvin"
-hi(x)
+Robot.say_hi(x)
+"""
+"""
+Correct way
+"""
+class Robot:
+
+    def __init__(self, name):
+        self.name = name
+
+    def hi(self):
+        print("Hello, I am", self.name)
+
+x = Robot('Marvin')
+x.hi()
